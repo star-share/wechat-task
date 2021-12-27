8ff028b02fc0cc4fe16f329cbd133cd347b67319
黎星 <lixing@kkguan.com>
2021-12-27 21:41:37 +0800

    error

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import onScan from "./event/scan";
 import onLogin from "./event/login";
 import onLogout from "./event/logout";
 import onMessage from "./event/message";
+import onError from "./event/error"
 
 const bot = WechatyBuilder.build({
   name: "star-share-bot",
@@ -13,8 +14,12 @@ bot.on("scan", onScan);
 bot.on("login", onLogin);
 bot.on("logout", onLogout);
 bot.on("message", onMessage);
+bot.on("error", onError);
 
 bot
   .start()
   .then(() => log.info("StarterBot", "Starter Bot Started."))
-  .catch((e) => log.error("StarterBot", e));
+  .catch(async (e) => {
+    log.error("Bot", "init() fail: %s", e);
+    process.exit(1);
+  })
